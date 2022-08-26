9d6cbb982ae420d9428594a94937b30f6fc45caf
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-26 19:52:36 -0300

    Criando fun~~

---
+++ Aumento.js
@@ -0,0 +1,10 @@
+
+
+PerguntarDados();
+console.log(`Nome - ${nome}`)
+console.log(`Salário - ${salario}`)
+
+function PerguntarDados(){
+    nome = prompt(`Qual o seu nome`)
+    salario = parseFloat(prompt(`Qual o seu salário`))    
+}
