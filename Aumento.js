0338b26d9de8c1da4c4a0b375781cc74e62f444f
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-26 21:11:05 -0300

    Atualizado com a função de calcular o aumento

--- Aumento.js
+++ Aumento.js
@@ -1,10 +1,35 @@
 
-
 PerguntarDados();
-console.log(`Nome - ${nome}`)
-console.log(`Salário - ${salario}`)
+
+
+if(salario <= 1500){
+    percentual = 20/100
+    console.log(percentual)
+}
+else if(salario <= 2000){
+    percentual = 15/100
+    console.log(percentual)
+}
+else if(salario <= 3000){
+    percentual = 10/100
+    console.log(percentual)
+}
+else{
+    percentual = 5/100
+    console.log(percentual)
+}
+
+CalcularAumento(salario, percentual);
+console.log(`Salário com aumento ${aumento}`)
+
+
+function CalcularAumento(num1, num2){
+    return aumento = num1 + (num1*num2) 
+}
 
 function PerguntarDados(){
     nome = prompt(`Qual o seu nome`)
-    salario = parseFloat(prompt(`Qual o seu salário`))    
+    salario = parseFloat(prompt(`Qual o seu salário`)) 
+    console.log(nome)   
+    console.log(salario)
 }
